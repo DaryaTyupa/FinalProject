f9059a2282f3beedcc83a98b0339be3416b25913
Darya Tyupa <91557639+DaryaTyupa@users.noreply.github.com>
2022-02-09 13:52:43 +0300

    Delete locators directory

--- locators/login_page_locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LoginPageLocators():
-
-    USERNAME_FIELD_LOCATOR = (By.XPATH, "//*[@id='id_username']")
-    PASSWORD_FIELD_LOCATOR = (By.XPATH, "//*[@id='id_password']")
-    LOGIN_BTN_LOCATOR = (By.XPATH, "//*[@value='Log in']")
